738edd69e7abb1871b3cda5709f19899987f266e
aimee rivers <aimee@aimeerivers.com>
2014-08-08 23:38:27 +0100

    Get it to listen to /o.gif and report stats.

--- app.js
+++ app.js
@@ -2,25 +2,23 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 var port = Number(process.env.PORT || 5000);
+var server = app.listen(port);
+var io = require('socket.io').listen(server);
 
 app.use(logfmt.requestLogger());
-
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
-
 app.use(express.static(__dirname + '/public'));
+app.enable('trust proxy');
 
 app.get('/', function(req, res) {
-  res.render("page");
+  res.render("index");
 });
 
-var io = require('socket.io').listen(app.listen(port));
+app.get('/o.gif', function(req, res) {
+  io.sockets.emit('newstats', { ip: req.ip, stat: req.url });
+  res.send(' ');
+});
+
 console.log("Listening on " + port);
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('message', { message: 'welcome to the chat' });
-  socket.on('send', function (data) {
-    io.sockets.emit('message', data);
-  });
-});
---
+++ public/stats.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+
+  var stats = [];
+  var socket = io.connect(window.location.hostname);
+  var statsDiv = document.getElementById("stats");
+
+  socket.on('newstats', function (data) {
+    if(data.stat) {
+      stats.push(data.stat);
+      var html = document.createElement('div');
+      var ip = document.createElement('strong');
+      ip.innerHTML = data.ip;
+      var content = document.createElement('p');
+      content.innerHTML = data.stat;
+      html.appendChild(ip);
+      html.appendChild(content);
+      statsDiv.appendChild(html);
+    } else {
+      console.log("There is a problem:", data);
+    }
+  });
+
+}
