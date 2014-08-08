2cadfbbb14e40ee575b02d80c1a7a37b1175d1ec
aimee rivers <aimee@aimeerivers.com>
2014-08-08 22:08:42 +0100

    NO WAY! I made a chat thing!

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var port = 5000;
 
 app.use(logfmt.requestLogger());
 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render("page");
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var io = require('socket.io').listen(app.listen(port));
+console.log("Listening on " + port);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('message', { message: 'welcome to the chat' });
+  socket.on('send', function (data) {
+    io.sockets.emit('message', data);
+  });
 });
