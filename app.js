63415b419a9eb72aebbf2e2e9ed7bdc154e7cc52
aimee rivers <aimee@aimeerivers.com>
2014-08-08 22:22:45 +0100

    I have a feeling heroku isn't gonna want port 5000.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-var port = 5000;
+var port = Number(process.env.PORT || 5000);
 
 app.use(logfmt.requestLogger());
 
