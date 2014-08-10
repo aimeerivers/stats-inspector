862ed376c0271ac253ff481357f4644930ed0ba1
aimee rivers <aimee@aimeerivers.com>
2014-08-10 18:19:55 +0100

    Make classes for different request types.

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,16 +1,15 @@
 window.onload = function() {
 
-  var stats = [];
   var socket = io.connect(window.location.hostname);
   var statsDiv = document.getElementById("stats");
 
   socket.on('newstats', function (data) {
     if(data.stat && data.ip) {
       if(data.ip === window.ip) {
-        stats.push(data.stat);
+        var stat = StatsRequest(data.stat);
         var html = document.createElement('div');
         var content = document.createElement('p');
-        content.innerHTML = data.stat;
+        content.innerHTML = stat.type + ' ' + stat.raw;
         html.appendChild(content);
         statsDiv.appendChild(html);
       }
@@ -19,4 +18,45 @@ window.onload = function() {
     }
   });
 
+}
+
+function StatsRequest(request) {
+  if(request.indexOf('/o.gif') === 0)
+    return IStatsRequest(request);
+
+  if(request.indexOf('/bbc/int/s') === 0 || request.indexOf('/bbc/bbc/s') === 0)
+    return DaxRequest(request);
+
+  if(request.indexOf('/e/') === 0)
+    return RdotRequest(request);
+
+  return BasicRequest(request);
+}
+
+function BasicRequest(request) {
+  return {
+    type: 'unknown',
+    raw: request
+  }
+}
+
+function IStatsRequest(request) {
+  return {
+    type: 'iStats',
+    raw: request
+  }
+}
+
+function DaxRequest(request) {
+  return {
+    type: 'DAx',
+    raw: request
+  }
+}
+
+function RdotRequest(request) {
+  return {
+    type: 'Rdot',
+    raw: request
+  }
 }
